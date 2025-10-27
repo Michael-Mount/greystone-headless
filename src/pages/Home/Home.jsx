b864e69567a5f4d07c115b0ce68183156e438856
Michael Mount <michaeljoesphmount@gmail.com>
2025-10-27 16:57:42 -0400

    built luxury drawing anmation component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HeroVIdeo from "../../components/Decorative/HeroVideo/HeroVIdeo";
+import LuxuryDraw from "../../components/Decorative/LuxuryDraw/LuxuryDraw";
 
 export default function Home() {
   return (
     <div>
       <HeroVIdeo />
+      <LuxuryDraw size={420} />
     </div>
   );
 }
