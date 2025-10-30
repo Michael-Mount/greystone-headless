7ceb682f4547c105927f705299557b5cc583d181
Michael Mount <michaeljoesphmount@gmail.com>
2025-10-30 11:52:33 -0400

    built scroll to top component to for web app

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
